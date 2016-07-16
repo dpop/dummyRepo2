d04033c7df128fb1dd99666c982fafb7a50c45da
Dragos <dpop1990@gmail.com>
2016-07-16 03:04:10 +0300

    create user update

--- admin/createUser.php
+++ admin/createUser.php
@@ -3,24 +3,28 @@ include_once '../vendor/autoload.php';
 session_start();
 use RedBeanPHP\R;
 
-echo getcwd();
-
-echo file_get_contents("../vendor/autoload.ph");
 //if (!isset($_SESSION["loggedIn"]) || !($_SESSION["loggedIn"] instanceof \RedBeanPHP\OODBBean) || $_SESSION["loggedIn"]->getProperties()["role"] != "admin")
 //     die("not logged in");
-
-R::setup('mysql:host=198.71.225.63;dbname=qbits_tek4kidz','tek4kidz','Aebaht4I');
-if (isset($_REQUEST["username"]) && isset($_REQUEST["password"]) && isset($_REQUEST["role"]))
+try
 {
 
-    $user = R::dispense("users");
-    $user->setProperty("username", $_REQUEST["username"]);
-    $user->setProperty("password", md5($_REQUEST["password"]));
-    $user->setProperty("role", $_REQUEST["role"]);
+    R::setup('mysql:host=198.71.225.63;dbname=qbits_tek4kidz','tek4kidz','Aebaht4I');
+    if (isset($_REQUEST["username"]) && isset($_REQUEST["password"]) && isset($_REQUEST["role"]))
+    {
 
-    R::store($user);
+        $user = R::dispense("users");
+        $user->setProperty("username", $_REQUEST["username"]);
+        $user->setProperty("password", md5($_REQUEST["password"]));
+        $user->setProperty("role", $_REQUEST["role"]);
+
+        R::store($user);
 
 
+    }
+
+    header('Location: \admin.php');
+}catch (Exception $e)
+{
+    print_r($e);
 }
 
-header('Location: \admin.php');
